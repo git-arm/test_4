67654593e0aa9b01249ec717721edb09662b7ce3
git-arm <111350495+git-arm@users.noreply.github.com>
2022-08-31 19:26:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 import pandas as pd
+#from oauth2client.service_account import ServiceAccountCredentials
 import datetime
+import requests
+from requests.exceptions import ConnectionError
+import bs4
+from bs4 import BeautifulSoup
+import dash
+from dash import dcc
+from dash import html
+#import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import plotly.graph_objs as go
+import plotly.express as px
 import numpy as np
+import pandas as pd
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import simpledialog
+#import xlrd
 import mysql.connector
-import datetime
-from flask import Flask, make_response, Response, json
-from flask import Flask, render_template, jsonify, redirect, request
-from datetime import datetime, time, timedelta
+import threading
+#import psycopg2
+import time
+#from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import select
 import json
+from configparser import ConfigParser
+#import goto
+#import label
+import pandas as pd
+import subprocess
+import datetime
+import pyodbc
+from flask import Flask, make_response, Response
+from flask import Flask, render_template, jsonify, redirect, request
+import threading
+from datetime import datetime, time, timedelta
+import csv
 
 meter_id = [1]
 
@@ -17,15 +49,59 @@ yesterday_midnight = midnight - timedelta(days=1)
 #print(f"from {yesterday_midnight.timestamp()}......to {midnight.timestamp()}")
 
 
+
+def mysqldatabase_read():
+    global x_value
+    global y_value
+    try:
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
+        db_cursor = db.cursor()
+        stmt1 = "SELECT voltage from bokaro_temp WHERE meterID = 1"
+        db_cursor.execute(stmt1)
+        data1 = db_cursor.fetchall()
+        db.commit()
+        db.close()
+        y_values = pd.DataFrame(data1)
+        #print((y_values.T).iloc[:,0:].values)
+        y_val = ((y_values.T).values.tolist())
+        #print((y_values.T).to_numpy())
+        #print(y_val[0])
+        y_value = y_val[0]
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+    try:
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
+        db2_cursor = db2.cursor()
+        stmt2 = "SELECT time_info from bokaro_temp WHERE meterID = 1"
+        db2_cursor.execute(stmt2)
+        data2 = db2_cursor.fetchall()
+        db2.commit()
+        db2.close()
+        x_values = pd.DataFrame(data2)
+        #print((y_values.T).iloc[:,0:].values)
+        x_val = ((x_values.T).values.tolist())
+        #print((y_values.T).to_numpy())
+        print(x_val[0])
+        x_value = x_val[0]
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+
+    time.sleep(5)
+    mysqldatabase_read()
+
+th1 = threading.Thread(target=mysqldatabase_read, daemon=True)
+#th1.start()
+
 app = Flask(__name__)
+
+
 
 
 @app.route('/data', methods =["GET", "POST"])
 def data():
     global meter_id
     try:
-        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db_cursor = db.cursor()
         stmt1 = "SELECT voltage from bokaro_1 WHERE meterID = %s"
         data1 = meter_id
@@ -43,8 +119,7 @@ def data():
     except mysql.connector.Error as err:
         print("Something went wrong: {}".format(err))
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT time_stamp from bokaro_1 WHERE meterID = %s"
         data2_m = meter_id
@@ -62,8 +137,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db_cursor = db.cursor()
         stmt1 = "SELECT current from bokaro_1 WHERE meterID = %s"
         data3_m = meter_id
@@ -81,8 +155,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db_cursor = db.cursor()
         stmt1 = "SELECT frequency from bokaro_1 WHERE meterID = %s"
         data4_m = meter_id
@@ -99,8 +172,7 @@ def data():
     except mysql.connector.Error as err:
         print("Something went wrong: {}".format(err))
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT total_harmonic_distortion from bokaro_1 WHERE meterID = %s"
         data2_m = meter_id
@@ -118,8 +190,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT apparent_energy from bokaro_1 WHERE meterID = %s"
         data2_m = meter_id
@@ -137,8 +208,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT power_factor from bokaro_1 WHERE meterID = %s"
         data2_m = meter_id
@@ -156,8 +226,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT energy from bokaro_1 WHERE meterID = %s"
         data2_m = meter_id
@@ -175,8 +244,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT power from bokaro_1 WHERE meterID = %s"
         data2_m = meter_id
@@ -194,8 +262,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT apparent_power from bokaro_1 WHERE meterID = %s"
         data2_m = meter_id
@@ -213,8 +280,7 @@ def data():
         print("Something went wrong: {}".format(err))
 
     try:
-        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db2 = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db2_cursor = db2.cursor()
         stmt2 = "SELECT location from bokaro_1_loc WHERE meterID = %s"
         data2_m = meter_id
@@ -246,14 +312,38 @@ def meter():
     meter_id = [int(request.form.get("meter_id"))]
     print(meter_id)
     print(type(meter_id))
-    return render_template('ems_test_v20.html')
+
+
+
+    try:
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
+        db_cursor = db.cursor()
+        stmt1 = "SELECT * from bokaro_1 "
+        data5_m = meter_id
+        db_cursor.execute(stmt1)
+        data7 = db_cursor.fetchall()
+        db.commit()
+        db.close()
+        b_values = pd.DataFrame(data7, columns=['meterID', 'time_stamp', 'voltage', 'current', 'frequency',
+                                                'total_harmonic_distortion', 'apparent_energy', 'power_factor',
+                                                'energy', 'power', 'apparent_power'])
+        # print((y_values.T).iloc[:,0:].values)
+        b_val = ((b_values).values.tolist())
+        # print((y_values.T).to_numpy())
+        # print(y_val[0])
+        b_value_c = b_val[0][0]
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+    response4 = make_response(json.dumps(b_val))
+    response4.content_type = 'applicatopn/json'
+
+    return render_template('ems_test_v21.html',data1=(b_val))
 
 
 @app.route("/getPlotCSV")
 def getPlotCSV():
     try:
-        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db_cursor = db.cursor()
         stmt1 = "SELECT * from bokaro_1 WHERE meterID =%s "
         data5_m = meter_id
@@ -296,11 +386,37 @@ def getPlotCSV():
                 "attachment; filename = BSL_EMS_meterdata.csv"})
 
 
+#@app.route("/getpopup")
+#def getpopup():
+#    try:
+#        db = mysql.connector.connect(user="python", password="Armatrics123$", host="192.168.0.108", database="jspl_wrm",
+#                                     port="3306")
+#        db_cursor = db.cursor()
+#        stmt1 = "SELECT * from jspl_wrm.bokaro_1 "
+#        data5_m = meter_id
+#        db_cursor.execute(stmt1)
+#        data6 = db_cursor.fetchall()
+#        db.commit()
+#        db.close()
+#        b_values = pd.DataFrame(data6, columns=['meterID', 'time_stamp', 'voltage', 'current', 'frequency'])
+        # print((y_values.T).iloc[:,0:].values)
+#        b_val = ((b_values.T).values.tolist())
+        # print((y_values.T).to_numpy())
+        # print(y_val[0])
+#        b_value_c = b_val[0][0]
+#    except mysql.connector.Error as err:
+#        print("Something went wrong: {}".format(err))
+#    response3 = make_response(json.dumps(b_val))
+#    response3.content_type = 'applicatopn/json'
+#    return response3
+
+
+
+
 @app.route("/getexcel")#, methods=["GET","POST"])
 def getexcel():
     try:
-        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db_cursor = db.cursor()
         stmt1 = "SELECT * from bokaro_1 "
         data5_m = meter_id
@@ -327,8 +443,7 @@ def getexcel():
 @app.route("/historical", methods=["POST"])
 def historical():
     try:
-        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db_cursor = db.cursor()
         stmt1 = "SELECT * from bokaro_1 "
         data5_m = meter_id
@@ -344,38 +459,101 @@ def historical():
         # print((y_values.T).to_numpy())
         # print(y_val[0])
         b_value_c = b_val[0][0]
+        b_val_map = list(map(json.dumps, b_val))
+
+        data0 = []
+        for i in range(1, 2):
+            for j in range(len(b_val)):
+                data0.append(b_val[j][i])
+
+
+
+        data1 = []                              #selecting the list elements
+        for i in range(2, 3):
+            for j in range(len(b_val)):
+                data1.append(b_val[j][i])
+
+        data2 = []
+        for i in range(3, 4):
+            for j in range(len(b_val)):
+                data2.append(b_val[j][i])
+
+        data3 = []
+        for i in range(4, 5):
+            for j in range(len(b_val)):
+                data3.append(b_val[j][i])
+
+        data4 = []
+        for i in range(5, 6):
+            for j in range(len(b_val)):
+                data4.append(b_val[j][i])
+
+        data5 = []
+        for i in range(6, 7):
+            for j in range(len(b_val)):
+                data5.append(b_val[j][i])
+
+        data6 = []
+        for i in range(7, 8):
+            for j in range(len(b_val)):
+                data6.append(b_val[j][i])
+
+        data7 = []
+        for i in range(8, 9):
+            for j in range(len(b_val)):
+                data7.append(b_val[j][i])
+
+        data8 = []
+        for i in range(9, 10):
+            for j in range(len(b_val)):
+                data8.append(b_val[j][i])
+
+        data9 = []
+        for i in range(10, 11):
+            for j in range(len(b_val)):
+                data9.append(b_val[j][i])
+
+
+
+
+
     except mysql.connector.Error as err:
         print("Something went wrong: {}".format(err))
-    response5 = make_response(json.dumps(b_val))
+    response5 = make_response(json.dumps(b_val_map))
     response5.content_type = 'applicatopn/json'
     #return response4
-    return render_template("historic.html", data=(b_val))
+    return render_template("historic.html", data=(b_val), data0=(data0), data1=(data1), data2=(data2), data3=(data3), data4=(data4), data5=(data5), data6=(data6), data7=(data7), data8=(data8), data9=(data9))
 
 @app.route("/specific_energy", methods=["GET","POST"])
 def sp_energy():
     try:
-        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase",
-                                         port="3306")
+        db = mysql.connector.connect(user="longingoatmeal9", password="4Ubrtd88hgU5YsHCJ9Ls9Q", host="server491829892.mysql.database.azure.com", database="blsdatabase", port="3306")
         db_cursor = db.cursor()
         stmt1 = "SELECT SUM(energy) from bokaro_1 WHERE time_stamp BETWEEN %s AND %s"
         data_allday_yesterday = (yesterday_midnight.timestamp(), midnight.timestamp())
         data5_m = meter_id
-        db_cursor.execute(stmt1,data_allday_yesterday)
+        db_cursor.execute(stmt1, data_allday_yesterday)
         data8 = db_cursor.fetchall()
         db.commit()
         db.close()
-
+        #b_values = pd.DataFrame(data8, columns=['meterID', 'time_stamp', 'voltage', 'current', 'frequency'])
+        # print((y_values.T).iloc[:,0:].values)
+        #b_val = ((b_values).values.tolist())
+        # print((y_values.T).to_numpy())
+        print(data8)
+        #b_value_c = b_val[0][0]
     except mysql.connector.Error as err:
         print("Something went wrong: {}".format(err))
     response4 = make_response(json.dumps(data8))
     response4.content_type = 'applicatopn/json'
-    return render_template('ems_test_v20.html', data=data8)
+    return render_template('ems_test_v21.html', data1=(data8))
 
 
 @app.route("/")
 def index():
-    return render_template('ems_test_v20.html')
-    
+    data='Please select a meter from home page'
+    return render_template('ems_test_v21.html', data0=(data))
+    #return render_template('datasheet.html')
 
 
 @app.route("/historic")
